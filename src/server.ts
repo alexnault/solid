7418b8cc93d48086a4678d42d74ded1bb15bae1a
Alex Nault <nault.alex@gmail.com>
2020-08-05 22:11:28 -0400

    Fix default port

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 5000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // const corsOptions = {
