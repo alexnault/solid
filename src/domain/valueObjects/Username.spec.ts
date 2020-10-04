c6bbd0b2f112f5f76d2a19abd22435477fdfe642
Alex Nault <nault.alex@gmail.com>
2020-10-03 23:23:09 -0400

    Add CreateUser service (#2)

--- src/domain/valueObjects/Username.spec.ts
+++ src/domain/valueObjects/Username.spec.ts
@@ -1,17 +1,17 @@
 import Username from "./Username";
 
 describe("Username", () => {
-  test("should throw username too short", async () => {
+  it("should throw username too short", async () => {
     expect(() => new Username("bob")).toThrow("Username too short");
   });
 
-  test("should throw username too long", async () => {
+  it("should throw username too long", async () => {
     expect(() => new Username("1234567890123456789012345678901")).toThrow(
       "Username too long"
     );
   });
 
-  test("should create a username", async () => {
+  it("should create a username", async () => {
     const username = new Username("myusername");
     expect(username.value).toEqual("myusername");
   });
--- src/repos/SequelizeUserRepo.ts
+++ src/repos/SequelizeUserRepo.ts
@@ -1,17 +1,28 @@
 import UserRepo from "./UserRepo";
 import User from "../domain/entities/User";
 import EntityId from "../shared/domain/EntityId";
+import Email from "../domain/valueObjects/Email";
+import Username from "../domain/valueObjects/Username";
 
 export default class SequelizeUserRepo implements UserRepo {
   // constructor() {}
+  async getUsers(): Promise<User[]> {
+    // TODO implement sequelize findAll
+    throw new Error("Not implemented");
+  }
 
   async getUserById(userId: EntityId): Promise<User> {
     // TODO implement sequelize findByPk
     throw new Error("Not implemented");
   }
 
-  async getUsers(): Promise<User[]> {
-    // TODO implement sequelize findAll
+  async getUserByEmail(email: Email): Promise<User> {
+    // TODO implement sequelize find
+    throw new Error("Not implemented");
+  }
+
+  async getUserByUsername(username: Username): Promise<User> {
+    // TODO implement sequelize find
     throw new Error("Not implemented");
   }
 
