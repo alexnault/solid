dbc4b3f4b5866e92b43efdecb9604cf9c3e0ce3a
Alex Nault <nault.alex@gmail.com>
2020-11-07 19:10:03 -0500

    Add tsconfig-paths to jest

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,11 @@
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+
+const { compilerOptions } = require("./tsconfig.json");
+
 module.exports = {
-  globals: {
-    "ts-jest": {
-      tsConfig: "tsconfig.json",
-    },
-  },
-  moduleFileExtensions: ["ts", "js"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
-  testMatch: ["**/*.spec.ts"],
+  preset: "ts-jest",
   testEnvironment: "node",
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
 };
